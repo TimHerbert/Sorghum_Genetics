84999c00ebb2546f469d3058473aa8a1264699b9
Tim Herbert <timothyherbert@hotmail.com>
2018-06-13 05:25:25 -0700

    Initial commit. Blank Template

---
+++ inc/footer.php
@@ -0,0 +1,4 @@
+ <!-- JS Files -->
+
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include_once('inc/header.php'); ?>
+
+
+    <main role="main" class="container">
+
+      <h1>Main content goes here</h1>
+
+    </main><!-- /.container -->
+
+<?php include_once('inc/footer.php'); ?>
